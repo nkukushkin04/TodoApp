16205f75e694f4a323d6bbb24d2d546f04e6c72e
user <user@K218PC9>
2022-10-12 14:05:37 +0300

    Добавьте файлы проекта.

---
+++ TodoApp/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using TodoApp.Models;
+using TodoApp.Services;
+using EO.WebBrowser.DOM;
+
+namespace TodoApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+        private BindingList<TodoModel> _todoDataList;
+        private FileIOService _fileIOService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIOService = new FileIOService(PATH);
+
+            _todoDataList = _fileIOService.LoadData
+
+            dgTodoList.ItemsSource = _todoDataList;
+            _todoDataList.ListChanged += _todoDataList_ListChanged;
+
+            }
+                new TodoModel(){Text="test" },
+                new TodoModel(){ Text = "djsad"},
+                new TodoModel(){ Text = "test 2", IsDone = true}
+            };
+
+        private void _todoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType== ListChangedType.ItemDeleted|| e.ListChangedType == ListChangedType.ItemChanged)
+            {
+
+            }
+
+            
+        }
+    }
+}
---
+++ TodoApp/Services/FileIOService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Models;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace TodoApp.Services
+{
+    class FileIOService
+    {
+        private readonly string PATH;
+        private object todoDataList;
+
+        public FileIOService()
+        {
+        }
+
+        public FileIOService(string path)
+        {
+            PATH = path;
+        }
+
+        public BindingList<TodoModel> LoadData()
+        {
+            var fileExists = File.Exists(PATH);
+            if (!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<TodoModel>();
+            }
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<TodoModel>>(fileText);
+            }
+        }
+
+        public void SaveData(BindingList<TodoModel> _todoDataList)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(todoDataList);
+                writer.Write(output);
+            }
+        }
+    }
+}
